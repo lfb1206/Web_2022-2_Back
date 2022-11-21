086b20770f96869509427dd5cced0134c8ba9693
lfb1206 <lucas.fernndez@uc.cl>
2022-11-21 10:32:16 -0300

    make it better

--- src/app.js
+++ src/app.js
@@ -14,12 +14,23 @@ const app = new Koa();
 app.context.orm = orm;
 
 // Por si hay problemas de cors
-app.use(cors({
-  origin: ['https://poetic-croissant-5e5bde.netlify.app'],
-  methods: ['GET', 'POST', 'DELETE'],
-  credentials: true,
-  origin: true,
-}));
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://poetic-croissant-5e5bde.netlify.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // Logs requests from server
 app.use(KoaLogger());
